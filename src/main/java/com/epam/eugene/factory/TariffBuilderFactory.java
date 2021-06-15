d3f260fe52bc74ff2cb4dc2659802608e12bf959
Eugene Shadura <evgeshashadura@gmail.com>
2021-06-15 17:48:35 +0300

    add handling exception

--- src/main/java/com/epam/eugene/factory/TariffBuilderFactory.java
+++ src/main/java/com/epam/eugene/factory/TariffBuilderFactory.java
@@ -1,11 +1,17 @@
 package com.epam.eugene.factory;
 
+import com.epam.eugene.exception.TariffException;
 import com.epam.eugene.parser.builder.AbstractTariffsBuilder;
 import com.epam.eugene.parser.builder.TariffsDomBuilder;
 import com.epam.eugene.parser.builder.TariffsSaxBuilder;
-import com.epam.eugene.parser.stax.TariffsStaxBuilder;
+import com.epam.eugene.parser.builder.TariffsStaxBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TariffBuilderFactory {
+    private static Logger logger = LogManager.getLogger();
+    private static final String MESSAGE_ERROR = "Type parser is invalid";
+
     private enum TypeParser {
         SAX, STAX, DOM
     }
@@ -13,21 +19,30 @@ public class TariffBuilderFactory {
     private TariffBuilderFactory() {
     }
 
-    public static AbstractTariffsBuilder createStudentBuilder(String typeParser) {
-        // insert parser name validation
-        TypeParser type = TypeParser.valueOf(typeParser.toUpperCase());
-        switch (type) {
-            case DOM -> {
-                return new TariffsDomBuilder();
+    public static AbstractTariffsBuilder createTariffBuilder(String typeParser)  {
+        TypeParser type = null;
+        try {
+            if (typeParser == null || typeParser.isEmpty()) {
+                logger.error(MESSAGE_ERROR);
+                throw new TariffException(MESSAGE_ERROR);
             }
-            case STAX -> {
-                return new TariffsStaxBuilder();
+            type = TypeParser.valueOf(typeParser.toUpperCase());
+            switch (type) {
+                case DOM -> {
+                    return new TariffsDomBuilder();
+                }
+                case STAX -> {
+                    return new TariffsStaxBuilder();
+                }
+                case SAX -> {
+                    return new TariffsSaxBuilder();
+                }
+                default -> throw new EnumConstantNotPresentException(
+                        type.getDeclaringClass(), type.name());
             }
-            case SAX -> {
-                return new TariffsSaxBuilder();
-            }
-            default -> throw new EnumConstantNotPresentException(
-                    type.getDeclaringClass(), type.name());
+        } catch (TariffException e) {
+            logger.error("Create tariff builder: " + e);
         }
+        return null;
     }
 }
